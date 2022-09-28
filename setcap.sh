b9e675fadf1ea5bd737a5985ced179758276979a
Mark Parker <msparker@sky.com>
2022-09-28 18:20:29 +0100

    update setcap

--- setcap.sh
+++ setcap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set access for python script to use port 80
-setcap 'cap_net_bind_service=+ep' /usr/bin/python3.9
+setcap 'cap_net_bind_service=+ep' /usr/bin/python3.10
 
 # Remove access for python script to use port 80
-#setcap -r /usr/bin/python3.9
+#setcap -r /usr/bin/python3.10
